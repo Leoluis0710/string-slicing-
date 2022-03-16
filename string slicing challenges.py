3a7096a0c4e777a48f9f8794f654c9c1e1eae8f1
Leoluis0710 <98952616+Leoluis0710@users.noreply.github.com>
2022-03-16 14:42:54 +0000

    Add files via upload

--- string slicing challenges.py
+++ string slicing challenges.py
@@ -1,2 +1,6 @@
-name = 'dsnjkfbailf hsdilrfh weoddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd'
-print(len(name))
+firstname = str(input('please enter your first name'))
+lastname = str(input('please enter your last name'))
+space = ' '
+concat = firstname + space + lastname
+print(concat[0:9])
+print(len(concat))
